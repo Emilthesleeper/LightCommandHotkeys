222db9aa3a84dee19926dd7870833a79a9930e30
Emilthesleeper <emilwege@icloud.com>
2025-11-23 14:35:39 +0100

    Initial commit

---
+++ src/client/java/com/emilsleeper/lightcommandhotkeys/client/LightCommandHotkeysClient.java
@@ -0,0 +1,10 @@
+package com.emilsleeper.lightcommandhotkeys.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LightCommandHotkeysClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/emilsleeper/lightcommandhotkeys/LightCommandHotkeys.java
@@ -0,0 +1,10 @@
+package com.emilsleeper.lightcommandhotkeys;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LightCommandHotkeys implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}

fcf2dcbfd02e09914513549286b388232b2a9877
Emilthesleeper <emilwege@icloud.com>
2025-12-02 20:34:10 +0100

    Created Branch 1.21.x

---
+++ src/client/java/com/emilsleeper/lightcommandhotkeys/client/ModMenuIntegration.java
@@ -0,0 +1,12 @@
+package com.emilsleeper.lightcommandhotkeys.client;
+
+import com.emilsleeper.lightcommandhotkeys.ConfigScreen;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return ConfigScreen::createConfigScreen;
+    }
+}
